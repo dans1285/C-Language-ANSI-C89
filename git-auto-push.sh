99d3773b57816c9f064c490a8fb1f26572019bf4
Daniel Sefu <danielsefu10@gmail.com>
2025-06-05 02:22:53 -0400

    Auto update on 2025-06-05 02:22:53

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd ~/C-90
+
+# Go to repo root
+cd "$(dirname "$0")"
+
+# Add all changes (but respects .gitignore)
 git add .
-git commit -m "Auto update on $(date)"
+
+# Commit with current date/time
+git commit -m "Auto update on $(date +"%Y-%m-%d %H:%M:%S")"
+
+# Push to origin main branch
 git push origin main
